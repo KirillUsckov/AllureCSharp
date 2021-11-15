b2bdcf144a6c3b46e833eaa1357fa8a8d3b91f1b
Kirill <Kirill@DESKTOP-E7KAIB8>
2021-11-15 23:08:03 +0300

    upd exe

---
+++ NUnitAllureProject/UpdateAllureTitle.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NUnitAllureProject
+{
+
+    public static class UpdateAllureTitle
+    {
+        [Test]
+        public static void Update()
+        {
+            var url = "P:\\VisualStudio\\CSharpAllureNunit\\NUnitAllureProject\\allure-report\\widgets\\summary.json";
+            var title = "A1QA Allure Report";
+            string str = File.ReadAllText(url);
+            str = str.Replace("Allure Report", title);
+            File.Delete(url);
+            File.WriteAllText(url, str);
+        }
+    }
+}
